6e1356c80dcd71cea6aef213bd7e5b11c897a741
rubenmoya <rmoyarodriguez@gmail.com>
2015-11-20 09:26:42 +0100

    Add concert and concert validation

---
+++ app/models/concert.rb
@@ -0,0 +1,15 @@
+class Concert < ActiveRecord::Base
+  validates :venue, presence: true
+  validates :venue, length: { maximum: 40 }
+
+  validates :city, presence: true
+  validates :city, length: { maximum: 40 }
+
+  validates :date, presence: true
+
+  validates :price, presence: true
+  validates :price, numericality: true
+
+  validates :description, presence: true
+  validates :description, length: { maximum: 300 }
+end
